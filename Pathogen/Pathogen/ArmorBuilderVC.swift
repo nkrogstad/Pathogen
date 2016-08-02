9ee86e0557a4c5a75181c0d32d8cd6009c27e2df
Nick Krogstad <nickkrogstad@gmail.com>
2016-08-01 21:50:16 -0700

    Swipe to clear added on armor slots

--- Pathogen/Pathogen/ArmorBuilderVC.swift
+++ Pathogen/Pathogen/ArmorBuilderVC.swift
@@ -13,7 +13,6 @@ class ArmorBuilderVC: UIViewController {
     // MARK: Properties
     @IBOutlet var armorBackgrounds : [UIView]!
     var delay : Double = 1.0
-    let swipe_clear = UISwipeGestureRecognizer()
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
@@ -28,12 +27,14 @@ class ArmorBuilderVC: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         for armor in armorBackgrounds {
-            UIView.animateWithDuration(0.5, delay: delay, options: UIViewAnimationOptions.CurveEaseOut, animations: {
+            UIView.animateWithDuration(0.35, delay: delay, options: .CurveEaseOut, animations: {
                 armor.center.x -= self.view.bounds.width
                 }, completion: nil)
-            delay+=0.2
-            swipe_clear.addTarget(self, action: "swipedView")
-            armor.addGestureRecognizer(swipe_clear)
+            delay+=0.1
+            
+            let clearSwipe = UISwipeGestureRecognizer(target: self, action: #selector(ArmorBuilderVC.swipedView(_:)))
+            clearSwipe.direction = .Right
+            armor.addGestureRecognizer(clearSwipe)
             armor.userInteractionEnabled = true
         }
     }
@@ -43,9 +44,29 @@ class ArmorBuilderVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func swipedView(){
-        let tapAlert = UIAlertController(title: "Swiped", message: "You just swiped the swipe view", preferredStyle: UIAlertControllerStyle.Alert)
-        tapAlert.addAction(UIAlertAction(title: "OK", style: .Destructive, handler: nil))
-        self.presentViewController(tapAlert, animated: true, completion: nil)
+    func swipedView(sender: UISwipeGestureRecognizer){
+        let senderTag = sender.view!.tag
+        let clearedLabel = sender.view!.viewWithTag((senderTag + 6))
+        
+        UIView.animateWithDuration(0.4, delay: 0, options: .CurveEaseOut, animations: {
+            for (var i = senderTag + 5; i > senderTag; i -= 1) {
+                let currentLabel = sender.view!.viewWithTag(i)
+                currentLabel!.hidden = true
+            }
+            sender.view!.center.x += (self.view.bounds.width / 1.25)
+            }, completion: nil)
+        
+        UIView.animateWithDuration(0.4, delay: 0.35, options: .CurveEaseOut, animations: {
+            clearedLabel!.hidden = false
+            clearedLabel!.alpha = 1.0
+            sender.view!.center.x -= (self.view.bounds.width / 1.25)
+            }, completion: {
+                (finished: Bool) -> Void in
+                
+                // 'Cleared' text fade out
+                UIView.animateWithDuration(1.0, delay: 0.0, options: .CurveLinear, animations: {
+                    clearedLabel!.alpha = 0
+                    }, completion: nil)
+        })
     }
 }
